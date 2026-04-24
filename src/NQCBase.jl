b66e16f21b70c12855931a558fb10c17d1896db2
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2026-04-24 14:00:06 +0200

    ✨ Quickly replicate supercells

--- src/NQCBase.jl
+++ src/NQCBase.jl
@@ -14,23 +14,32 @@ include("cells.jl")
 """
     Structure{T}
 
-Structures are storage types to keep atoms, positions, cells and further information in one place. 
-Many functions in the NQCD packages use only parts of the structure information for efficiency, e.g. integration routines, but it can be convenient to have it all in one type. 
+Structures are storage types to keep atoms, positions, cells and further information in one place.
+Many functions in the NQCD packages use only parts of the structure information for efficiency, e.g. integration routines, but it can be convenient to have it all in one type.
 
-If you are developing in NQCD, please include multiple dispatch versions of your functions using Structure types where this would be convenient to the user. 
+If you are developing in NQCD, please include multiple dispatch versions of your functions using Structure types where this would be convenient to the user.
 """
 struct Structure
     atoms::NQCBase.Atoms # Atoms object
     positions::AbstractMatrix # Positions matrix in atomic units, each column containing one atom's positions
     cell::AbstractCell # Unit cell object
-    info::Dict{String, Any} # Other structure information, e.g. from an ExtXYZ header
+    info::Dict{String,Any} # Other structure information, e.g. from an ExtXYZ header
 end
 function Structure(atoms::NQCBase.Atoms, positions::AbstractMatrix, cell::AbstractCell)
     @assert length(atoms.types) == size(positions, 2) "Size of Positions needs to match number of Atoms. "
     if isa(cell, PeriodicCell)
         @assert size(cell.vectors, 1) == size(positions, 1) "Unit cell vectors must have the same dimensionality as Positions. "
     end
-    return Structure(atoms, positions, cell, Dict{String, Any}())
+    return Structure(atoms, positions, cell, Dict{String,Any}())
+end
+
+"""
+    (supercell::Supercell)(structure::Structure)
+
+    Shortcut for supercell replication of a structure. Applies the supercell operation to the positions of the structure, and replicates the atoms accordingly.
+"""
+function (supercell::Supercell)(structure::Structure)
+    return NQCBase.Structure(Atoms(repeat(structure.atoms.types, length(supercell.replicas))), supercell(structure.positions), structure.cell, structure.info)
 end
 
 # I/O Interfaces
